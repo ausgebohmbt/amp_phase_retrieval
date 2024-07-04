75707979062d114da8a2edb54b5a4302a8df83dd
ausgebohmbt <karad@iesl.forth.gr>
2024-07-04 16:25:08 +0800

    phase calibration: pre-loop cleanup

--- tests/phase_calibration_byGratting_autonoma.py
+++ tests/phase_calibration_byGratting_autonoma.py
@@ -25,31 +25,6 @@ if saVe_plot:
     if not os.path.exists(save_path):
         os.mkdir(save_path)
 
-
-"phase"
-phagen.modDepth = 220
-phagen.whichphuzzez = {"grating": True, "lens": False, "phase": False, "amplitude": False, "corr_patt": True,
-                       "lg_pol": False, "corr_phase": False}
-phagen.linear_grating()
-phagen._make_full_slm_array()
-slm_phase = phagen.final_phuz
-
-# fi = plt.figure()
-# plt.subplot(121)
-# plt.imshow(slm_phase_init, cmap='inferno')
-# plt.title("res_resz")
-# plt.colorbar(fraction=0.046, pad=0.04)
-# plt.subplot(122)
-# plt.imshow(circular_aperture, cmap='inferno')
-# plt.title("inv_zator_norm")
-# plt.colorbar(fraction=0.046, pad=0.04)
-# plt.show(block=False)
-# # fig.savefig(path + '\\iter_{}'.format(i) + '_full.png', dpi=300, bbox_inches='tight', transparent=False)
-# # Save data
-# # np.save(path + '\\imgF_iter_{}'.format(i), imgF)
-# plt.pause(0.8)
-# plt.close(fi)
-
 "init cam"
 exp = 1
 params = {'exposure': exp/1000, "initCam": True,
@@ -59,12 +34,10 @@ cam_obj = Cam.LiveHamamatsu(**params)
 slm_disp_obj = slm
 
 "prep cam"
+frame_num = 1
 cam_obj.mode = "Acq"
 cam_obj.num = 1
 cam_obj.bin_sz = 1
-# cam_roi_pos = [175, 1400]
-# cam_roi_sz = [300, 300]
-
 cam_obj.hcam.setACQMode('fixed_length', number_frames=cam_obj.num)
 cam_obj.exposure
 
@@ -75,8 +48,7 @@ cam_obj.roi_set_roi(int(cam_roi_pos[0] * cam_obj.bin_sz), int(cam_roi_pos[1] * c
                     int(cam_roi_sz[0] * cam_obj.bin_sz), int(cam_roi_sz[1] * cam_obj.bin_sz))
 
 "record background"
-
-print(Fore.LIGHTGREEN_EX + "record background" + Style.RESET_ALL)
+print(Fore.LIGHTGREEN_EX + "sha' record background" + Style.RESET_ALL)
 
 "close shutter"
 sh.shutter_state()
@@ -88,7 +60,7 @@ sh.shutter_state()
 
 time.sleep(0.5)
 
-frame_num = 1
+"set camera"
 cam_obj.stop_acq()
 cam_obj.take_average_image(frame_num)
 cam_obj.bckgr = copy.deepcopy(cam_obj.last_frame)
@@ -114,6 +86,23 @@ if sh.shut_state == 0:
 time.sleep(0.4)
 sh.shutter_state()
 
+"prep phase basics"
+phagen.whichphuzzez = {"grating": True, "lens": False, "phase": False, "amplitude": False, "corr_patt": True,
+                       "lg_pol": False, "corr_phase": False}
+phagen.linear_grating()
+
+"run the loop"
+# todo: can I replace the original take_average_image in the Cam Class with a new one that does
+#  not prep every time
+
+"phase"
+phagen.modDepth = 220
+phagen._make_full_slm_array()
+
+"up to slm"
+slm_disp_obj.display(phagen.final_phuz)
+
+"get image"
 cam_obj.take_average_image(frame_num)
 img = cam_obj.last_frame
 img_noBg = cam_obj.last_frame - bckgr
@@ -121,81 +110,13 @@ img_noBg = cam_obj.last_frame - bckgr
 "stack em"
 stack_sh_fk = np.concatenate((img_noBg, img, bckgr), axis=0)
 
-plo_che = False
-if plo_che:
-    fig = plt.figure()
-    plt.subplot(221), plt.imshow(img_noBg, cmap='inferno', vmin=0, vmax=1450)
-    plt.colorbar(fraction=0.046, pad=0.04)
-    plt.title('no bckgr: {}'.format(4))
-    plt.subplot(222), plt.imshow(img, cmap='inferno', vmax=2600)
-    plt.colorbar(fraction=0.046, pad=0.04)
-    plt.title('original img: {}'.format(2))
-    plt.subplot(223), plt.imshow(bckgr, cmap='inferno', vmax=600)
-    plt.colorbar(fraction=0.046, pad=0.04)
-    plt.title('bckgr')
-    plt.subplot(224), plt.imshow(stack_sh_fk, cmap='inferno', vmax=600)
-    plt.colorbar(fraction=0.046, pad=0.04)
-    plt.title('stack_sh_fk')
-    plt.show()
-    # plt.show(block=False)
-    # fig.savefig(path + '\\iter_{}'.format(i) + '_full.png', dpi=300, bbox_inches='tight', transparent=False)
-    # Save data
-    # np.save(path + '\\imgF_iter_{}'.format(i), imgF)
-    plt.pause(0.8)
-    plt.close(fig)
-
-"run the loop"
-# todo: can I replace the original take_average_image in the Cam Class with a new one that does
-#  not prep every time
-
-nossing = np.zeros((1024, 1272))
-
-"upload blank 2 slm"
-slm_disp_obj.display(nossing)
-cam_obj.take_average_image(frame_num)
-imgzaz_tipota = cam_obj.last_frame - bckgr
-
-slm_disp_obj.display(slm_phase)
-cam_obj.take_average_image(frame_num)
-imgzaz = cam_obj.last_frame - bckgr
-
 "gotta keep em separated"
-# separate_graph_regions(stack, graph, saVe_path, modDepth, crop_sz=2, saVe_plo=False):
 amps_primus, amps_nulla, amps_secundus = separate_graph_regions(stack_sh_fk, img_noBg,
                                                                 save_path, modDepth=phagen.modDepth,
                                                                 crop_sz=10, saVe_plo=saVe_plot)
 
+
 # "show em"
-# rezs = plt.figure()
-# plt.subplot(231)
-# plt.imshow(np.log10(stack_sh_fk), cmap='inferno')
-# plt.title("log o' stack")
-# plt.colorbar(fraction=0.046, pad=0.04)
-# plt.subplot(232)
-# plt.plot(stack_sh_fk[:, 446])
-# plt.plot(stack_sh_fk[:, 1010])
-# plt.plot(stack_sh_fk[:, 1568])
-# plt.title("profs")
-# plt.legend(["2nd order", "1st order", "0th order"])
-# plt.subplot(233)
-# plt.plot(imgzaz_tipota[50, :])
-# plt.plot(imgzaz[50, :])
-# plt.title("profile at {}".format(50))
-# plt.legend(["empty phase", "corretion & grating"])
-# plt.subplot(234)
-# plt.plot(imgzaz[50, :] - imgzaz_tipota[50, :])
-# plt.title("diff of prof @ {}".format(50))
-# plt.subplot(235)
-# plt.imshow(nossing, cmap='inferno')
-# plt.title("nossing")
-# plt.colorbar(fraction=0.046, pad=0.04)
-# plt.subplot(236)
-# # plt.imshow(stack_primus, cmap='inferno')
-# # # plt.imshow(slm_phase, cmap='inferno')
-# plt.title("phase, bitness {}".format(phagen.modDepth))
-# # plt.colorbar(fraction=0.046, pad=0.04)
-# plt.tight_layout()
-# # plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
-# plt.show()
+
 
 # es el final
